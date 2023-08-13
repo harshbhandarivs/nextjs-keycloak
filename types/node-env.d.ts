4ca57945aa45b6e9a8473065a1f0fec7c0a2e2e6
Harsh Bhandari <harsh.bhandari.v@gmail.com>
2023-08-13 15:55:17 +0530

    Add NEXTAUTH_URL and NEXTAUTH_SECRET to ProcessEnv type

--- types/node-env.d.ts
+++ types/node-env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    NEXTAUTH_URL: string
+    NEXTAUTH_SECRET: string
     KEYCLOAK_CLIENT_ID: string
     KEYCLOAK_CLIENT_SECRET: string
     KEYCLOAK_ISSUER: string
