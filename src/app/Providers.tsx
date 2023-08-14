ca987b3b918478e8fcd17e8a19c4597712019413
Harsh Bhandari <harshb@sahaj.ai>
2023-08-14 13:12:41 +0530

    Move time utility function from type directory to utils directory

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react"
 import { ReactNode } from "react"
-import { minutesInSeconds } from "../../types/time"
+import { minutesInSeconds } from "../utils/time"
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
