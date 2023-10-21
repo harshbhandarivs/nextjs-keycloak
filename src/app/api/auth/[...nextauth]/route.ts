804200d191e3787654bca518ab8d76b91b24fa86
Harsh Bhandari <harshb@sahaj.ai>
2023-10-22 03:12:11 +0530

    Refactoring changes in next-auth route file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,21 @@
 import { AuthOptions, TokenSet } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import NextAuth from "next-auth/next";
 import KeycloakProvider from "next-auth/providers/keycloak"
+
+function requestRefreshOfAccessToken(token: JWT) {
+  return fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_id: process.env.KEYCLOAK_CLIENT_ID,
+      client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken!,
+    }),
+    method: "POST",
+    cache: "no-store"
+  });
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -26,33 +41,25 @@ export const authOptions: AuthOptions = {
         token.refreshToken = account.refresh_token
         token.expiresAt = account.expires_at
         return token
-      } else if (Date.now() < (token.expiresAt! * 1000 - 60 * 1000)) {
+      }
+      if (Date.now() < (token.expiresAt! * 1000 - 60 * 1000)) {
         return token
       } else {
         try {
-          const response = await fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams({
-              client_id: process.env.KEYCLOAK_CLIENT_ID,
-              client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
-              grant_type: "refresh_token",
-              refresh_token: token.refreshToken!,
-            }),
-            method: "POST",
-            cache: "no-store"
-          })
+          const response = await requestRefreshOfAccessToken(token)
 
           const tokens: TokenSet = await response.json()
 
           if (!response.ok) throw tokens
 
-          return {
+          const updatedToken: JWT = {
             ...token, // Keep the previous token properties
             idToken: tokens.id_token,
             accessToken: tokens.access_token,
             expiresAt: Math.floor(Date.now() / 1000 + (tokens.expires_in as number)),
             refreshToken: tokens.refresh_token ?? token.refreshToken,
           }
+          return updatedToken
         } catch (error) {
           console.error("Error refreshing access token", error)
           return { ...token, error: "RefreshAccessTokenError" }
